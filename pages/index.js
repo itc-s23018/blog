a93dd1bcaf510caff0b9f7de7c9a204e4d9fc6e6
itc-s23018 <s23018@std.it-college.ac.jp>
2023-12-14 07:44:15 +0000

    2.9 p63
    ヒーローの画像をpropsでオンにする

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import Hero from 'components/hero'
 
 const Home = () => {
   const props = { title: 'CUBE', subtitle: 'アウトプットしていくサイト' }
-  return <Hero {...props} />
+  return <Hero {...props} imageOn />
 }
 export default Home
