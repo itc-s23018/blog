79fb6e2c71e194bce081b6b7351dabb97468943d
itc-s23018 <s23018@std.it-college.ac.jp>
2023-12-22 12:34:13 +0000

    6.4 サイトに関する情報を共有できる形で用意

--- components/meta.js
+++ components/meta.js
@@ -1,10 +1,14 @@
 import Head from 'next/head'
 
+import { siteMeta } from 'lib/constants'
+const { siteTitle, siteDesc, siteUrl, siteLocal, siteType, siteIcon } = siteMeta
+
 const Meta = ({ pageTitle }) => {
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
   return (
     <Head>
-      <title>{pageTitle}</title>
-      <mata prpperty='og:title' content={pageTitle} />
+      <title>{title}</title>
+      <mata prpperty='og:title' content={title} />
     </Head>
   )
 }
