92fbf860153d23f811208fa8f177c2012586898b
itc-s23018 <s23018@std.it-college.ac.jp>
2024-01-17 08:01:10 +0000

    9.5完了

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,15 +1,29 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   const props = { title: 'Blog', subtitle: 'Recent Posts' }
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
       <Hero {...props} />
+      <Posts posts={posts} />
     </Container>
   )
 }
 
+const getStaticProps = async () => {
+  const posts = await getAllPosts()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 export default Blog
+export { getStaticProps }
