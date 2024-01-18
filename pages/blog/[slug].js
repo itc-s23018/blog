4019d4f37b0909785698cd483b89fa5a86f3292a
itc-s23018 <s23018@std.it-college.ac.jp>
2024-01-18 07:06:11 +0000

    fallbackをblockingに設定して最新５件の記事ページだけを静的生成

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -75,7 +75,7 @@ const Post = ({
 }
 
 const getStaticPaths = async () => {
-  const allSlugs = await getAllSlugs()
+  const allSlugs = await getAllSlugs(5)
   return {
     paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
     fallback: false
@@ -85,27 +85,30 @@ const getStaticProps = async context => {
   const slug = context.params.slug
 
   const post = await getPostBySlug(slug)
+  if (!post) {
+    return { notFound: true }
+  } else {
+    const description = extractText(post.content)
 
-  const description = extractText(post.content)
+    const eyecatch = post.eyecatch ?? eyecatchLocal
 
-  const eyecatch = post.eyecatch ?? eyecatchLocal
+    const { base64 } = await getPlaiceholder(eyecatch.url)
+    eyecatch.blurDataURL = base64
 
-  const { base64 } = await getPlaiceholder(eyecatch.url)
-  eyecatch.blurDataURL = base64
+    const allSlugs = await getAllSlugs()
+    const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
 
-  const allSlugs = await getAllSlugs()
-  const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
-
-  return {
-    props: {
-      title: post.title,
-      publish: post.publishDate,
-      content: post.content,
-      eyecatch,
-      categories: post.categories,
-      description,
-      prevPost,
-      nextPost
+    return {
+      props: {
+        title: post.title,
+        publish: post.publishDate,
+        content: post.content,
+        eyecatch,
+        categories: post.categories,
+        description,
+        prevPost,
+        nextPost
+      }
     }
   }
 }
