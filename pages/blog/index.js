7511ba0393e965a20d49fa1b635cdcfb3f441616
itc-s23018 <s23018@std.it-college.ac.jp>
2024-01-18 02:42:18 +0000

    9.6完了

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,15 +1,40 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
 
-const Blog = () => {
+import { eyecatchLocal } from 'lib/constants'
+
+const Blog = ({ posts }) => {
   const props = { title: 'Blog', subtitle: 'Recent Posts' }
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
       <Hero {...props} />
+      <Posts posts={posts} />
     </Container>
   )
 }
 
+const getStaticProps = async () => {
+  const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 export default Blog
+export { getStaticProps }
